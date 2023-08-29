d690318536a67e3d9b6c09fc485349e9aa374fa5
Nusa Stegnar <nkrapez@gmail.com>
2023-08-29 06:45:38 +0200

    new

---
+++ meow.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    int i = 0;
+    while (i < 3)
+    {
+        printf("meow\n");
+        i++
+    }
+}
+
+
+
+#include <stdio.h>
+
+int main(void)
+
+for (int i = 0; i < 3; i++)
+{
+    printf("meow\n");
+}
+
+
+
+#include <stdio.h>
+
+int main(void)
+
+while (true)
+{
+    printf("meow\n")
+}
---
+++ phonebook.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("What's your name? ");
+    int age = get_int("What's your age? ");
+    string number = get_string("What's your phone number? ");
+
+    printf("Age is %i. Name is %s. Phone number is %s.", age, name, number);   
+}
+
